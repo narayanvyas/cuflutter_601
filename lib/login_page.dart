c2700c540e0b8ae3b2beaae57e42043e6b7306b3
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-02-03 11:12:42 +0530

    Working on Login Page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+
+bool validateForm() {
+  if (emailController.text.length < 4) {
+    print('Email is incorrect');
+    return false;
+  } else if (passwordController.text.length < 4) {
+    print('Password is too short');
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void performLogin() {
+  if (validateForm()) {
+    print('Login Success');
+  }
+}
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -16,9 +38,12 @@ class LoginPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Column(
+        child: ListView(
           children: [
+            Lottie.network(
+                'https://lottie.host/61e94b29-0c4f-4524-b787-927399d289ed/U4Vt6SLuq5.json'),
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                   hintText: 'Email', border: OutlineInputBorder()),
             ),
@@ -26,13 +51,18 @@ class LoginPage extends StatelessWidget {
               height: 30,
             ),
             TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                   hintText: 'Password', border: OutlineInputBorder()),
             ),
             Container(
               height: 30,
             ),
-            ElevatedButton(onPressed: () {}, child: Text('Login'))
+            ElevatedButton(
+                onPressed: () {
+                  performLogin();
+                },
+                child: Text('Login'))
           ],
         ),
       ),
